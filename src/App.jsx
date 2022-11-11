7d74a273a828ee5a38d4d4ed44a0b7c7b6aa3e3e
Ömer Can Korkmaz <omercankorkmaz0@gmail.com>
2022-11-11 11:55:46 +0300

    prime react added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import './assets/global.css';
+import { Button } from 'primereact/button';
 
 function App() {
-    return <div className="App">INITIAL</div>;
+    return (
+        <div className="App">
+            <Button label="INITIAL" loading loadingIcon="pi pi-spin pi-sun" />
+        </div>
+    );
 }
 
 export default App;
